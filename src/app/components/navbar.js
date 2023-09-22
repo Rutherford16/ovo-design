6431f0b71ec802118da511ae0636660fada97e9b
Rutherford16 <hidayatronny@ymail.com>
2023-09-22 13:38:38 +0800

    update

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -10,17 +10,17 @@ export default function Navbar() {
                 </svg>
                 Home
             </Link>
-            <Link href='/' className='flex flex-col items-center p-1'>
+            <Link href='/dashboard' className='flex flex-col items-center p-1'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
                 Finance
             </Link>
-            <Link href='/' className='flex flex-col items-center p-1 static w-16'>
-                <Image src='/images/QRIS_logo.svg' width={55} height={55} alt='QRIS' className="bg-gradient-to-b from-purple-400 to-purple-800 rounded-full fill-white w-16 h-16 p-2 absolute -top-8 border-4 border-white" />
+            <Link href='/dashboard' className='flex flex-col items-center p-1 static w-16'>
+                <Image src='/ovo-design/images/QRIS_logo.svg' width={55} height={55} alt='QRIS' className="bg-gradient-to-b from-purple-400 to-purple-800 rounded-full fill-white w-16 h-16 p-2 absolute -top-8 border-4 border-white" />
                 <span className="absolute top-7">Pay</span>
             </Link>
-            <Link href='/' className='flex flex-col items-center p-1'>
+            <Link href='/dashboard' className='flex flex-col items-center p-1'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H6.911a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661z" />
                 </svg>
